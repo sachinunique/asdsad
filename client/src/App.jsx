b142d95e8ae8cbe1e6efdf28ca68d55d3cef57b9
unknown <sachin.uppcl@yahoo.com>
2025-12-09 08:03:11 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+ import ErrorPages from './Pages/ErrorPages';
+ import Home from './Pages/Home';
+  import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Dashboard from './Pages/Dashboard';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Dashboard/>,
+        errorElement:<ErrorPages/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            },
+            {
+              path:"/about",
+              element:<About/>
+            },
+             {
+              path:"/login",
+              element:<SignIn/>
+            },
+            {
+              path:"/about",
+              element:<About/>
+            },
+             {
+              path:"/project",
+              element:<Projects/>
+            },
+            
+        ]
+
+    },
+    
+  ])
   return (
-   < h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <RouterProvider router={router}></RouterProvider>
   )
 }
 
